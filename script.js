6caf49fcbd1d6fa8dd4ccb6557e32606e2f4f94c
vishalcangit <vishalsuvarna000@gmail.com>
2022-07-14 16:02:20 +0530

    done with styles

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// imports search bar
 const search_bar = document.getElementById("search-bar");
+// imports submit /search button
 const search_btn = document.getElementById("search-btn");
+// imports show favourite button
 const favourites = document.getElementById("show-fav");
+// imports meal container
 const mealsContainer = document.getElementById("meals");
+// imports the heading of search results
 const resultHeading = document.getElementById("search-result-info");
+// imports single meal container
 const single_meal_el = document.getElementById("single-meal");
+// imports favourite list (aside one)
 const favList = document.getElementById("fav-list");
-// const body = document.querySelector("body");
+// makes an empty array of name meals 
 let meals = [];
 
 // searchh meal fn
@@ -17,7 +24,6 @@ function searchMeal(e) {
 
     // get search meal results
     const searchValue = search_bar.value;
-    console.log(searchValue);
 
     // check for empty searchh bar
     if (searchValue.trim()) {
@@ -25,7 +31,6 @@ function searchMeal(e) {
         )
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
                 resultHeading.innerHTML = `<h2>Search results for : ${searchValue}</h2>`
                 if (data.meals === null) {
                     resultHeading.innerHTML = `<h2>There are no such items as ${searchValue}</h2>`
@@ -60,7 +65,7 @@ function getMealById(mealId) {
             addMealToDOM(meal);
         })
 }
-// adding meal to dom 
+// adding meal to dom when clicked in meals name
 function addMealToDOM(meal) {
     const ingredients = [];
     for (let i = 1; i <= 20; i++) {
@@ -89,21 +94,23 @@ function addMealToDOM(meal) {
         </div>
     </div>`
 }
-
+// function to open box when clicked in meal name
 function openBox() {
     document.getElementById("single-meal").classList.remove("inactive");
 }
+// function to open box when clicked in close button
 function closeBox() {
     document.getElementById("single-meal").classList.add("inactive");
 }
-
+// function to open favourites when clicked in show fav
 function showFav() {
     document.getElementById("aside").classList.remove("inactive")
 }
+// function to close favourites when clicked in close button
 function closeFav() {
     document.getElementById("aside").classList.add("inactive")
 }
-
+// function to add the meals to favourite list 
 function addToFav(e) {
     const mealInfo = e.composedPath().find((item) => {
         if (item.classList) {
@@ -127,20 +134,19 @@ function addToFav(e) {
                 // Push to global Array
                 meals.push(storeMeal);
                 window.localStorage.setItem('meal', JSON.stringify(meals));
-
                 let div = document.createElement('div');
                 div.innerHTML = `
-            <div class='fav-container'>
-              <img src = '${storeMeal.thumbnail}' alt = '${storeMeal.strMeal}' />
-              <h1 class='heading'>${storeMeal.heading}</h1>
-              <span class="del-fav" onclick="deleteFav(event)"><i class="fa-regular fa-trash-can"></i></span>
-              </div>`
-                // Rendering on DOM
+                    <div class='fav-container'>
+                        <img src = '${storeMeal.thumbnail}' alt = '${storeMeal.strMeal}' />
+                        <h1 class='heading'>${storeMeal.heading}</h1>
+                        <span class="del-fav" onclick="deleteFav(event)"><i class="fa-regular fa-trash-can"></i></span>
+                    </div>`
+                // Rendering on DOM element
                 favList.appendChild(div);
             });
     }
 }
-
+// this delete function will delete an item fro meal array stored in the local storage
 function deleteFav(e) {
     console.log(e);
     const mealInfo = e.composedPath().find((item) => {
@@ -164,11 +170,11 @@ function deleteFav(e) {
     mealInfo.remove();
 }
 
+// this function will load the arra saved with meal and it willl load on refresh without getting the favourite list getting deleted
 function initialize() {
     const mealsData = JSON.parse(window.localStorage.getItem('meal'));
-
+    // spread operator used to get the meals to run in for loop
     meals = [...mealsData];
-
     for (let i = 0; i < meals.length; i++) {
         let getMeal = meals[i];
         console.log(getMeal);
@@ -185,10 +191,15 @@ function initialize() {
 };
 
 
-// events listeners
+// events listeners to search button
 search_btn.addEventListener("click", searchMeal);
 
+// event listener for meal clicked 
 mealsContainer.addEventListener("click", (e) => {
+
+    // clear previous search
+    single_meal_el.innerHTML = "";
+
     const mealInfo = e.composedPath().find((item) => {
         if (item.classList) {
             return item.classList.contains("meal-info");
